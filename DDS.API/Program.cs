d6cbc641a438c9089142852b3a4a0366cc727f65
Thomas Peire <thomas.peire@outlook.com>
2023-07-11 19:34:33 +0200

    wip

--- DDS.API/Program.cs
+++ DDS.API/Program.cs
@@ -1,68 +1,25 @@
-using System.Text;
-using DDS.Modules.Customers.Configuration;
+﻿using DDS.Modules.Customers.Configuration;
 using DDS.Modules.Customers.Domain;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var config = builder.Configuration;
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services
     .AddCustomerModule(config)
     .AddGraphQLServer()
     .AddLocalSchema("Customers");
-
-
-// builder.Services
-    // .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    // .AddMicrosoftAccount(options =>
-    // {
-    //     options.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"]!;
-    //     options.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"]!;
-    //     options.SaveTokens = true;
-    // })
-    // .AddCookie();
-
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//     .AddJwtBearer(options =>
-//     {
-//         options.Authority = $"https://login.microsoftonline.com/{Configuration["AzureAd:TenantId"]}/v2.0"; // where AzureAd:TenantId is your Tenant ID
-//         options.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             ValidateIssuer = true,
-//             ValidIssuer = $"https://login.microsoftonline.com/{Configuration["AzureAd:TenantId"]}/v2.0",
-//             ValidateAudience = true,
-//             ValidAudience = Configuration["AzureAd:ClientId"], // where AzureAd:ClientId is your Client ID
-//             ValidateLifetime = true
-//         };
-//     });
-
-
-
-// builder.Services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
-// {
-//     options.TokenValidationParameters = new TokenValidationParameters
-//     {
-//         ValidateIssuer = true,
-//         ValidateAudience = true,
-//         ValidateLifetime = true,
-//         ValidateIssuerSigningKey = true,
-//
-//         ValidIssuer = "thomas",
-//         ValidAudience = "builder.Configuration[\"Jwt:Audience\"]",
-//         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["testkey"]))
-//     };
-// });
-
-
-
-// builder.Services.AddAuthorization();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -74,24 +31,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// app.UseAuthentication();
-// app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapGet("/secure", [Authorize] () => "Secure endpoint!");
+app.MapGet("/", (HttpContext context) =>
+{
+    return context.User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+})
+    .RequireAuthorization();
 
-// app.MapGet("/", (HttpContext context) =>
-// {
-//     //context.GetTokenAsync("access_token");
-//     return context.User.Claims.Select(c => new { c.Type, c.Value }).ToList();
-// });
-// app.MapGet("/login", () =>
-//     Results.Challenge(
-//         new AuthenticationProperties()
-//         {
-//             RedirectUri = "https://localhost:5000/"
-//         },
-//         authenticationSchemes: new List<string> { MicrosoftAccountDefaults.AuthenticationScheme }));
-app.MapPost("/new", async (ICustomerRepository repository) => await CreateCustomer(repository));
+app.MapPost("/createCustomer", async (ICustomerRepository repository) => await CreateCustomer(repository))
+    .WithName("CreateCustomer")
+    .WithOpenApi()
+    .RequireAuthorization(); ;
 app.MapGet("/customer/{id:guid}",
-    async (ICustomerRepository repository, Guid id) => await repository.LoadAsync<Customer>(id));
+    async (ICustomerRepository repository, Guid id) => await repository.LoadAsync<Customer>(id))
+    .WithName("GetCustomer")
+    .WithOpenApi()
+    .RequireAuthorization();
 
 app.MapGraphQL();
 app.Run();
